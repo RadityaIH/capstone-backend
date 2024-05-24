0614ef6091a8b81c4089fc45103faa6160373be1
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2024-05-24 10:06:58 +0700

    fix problems

--- app/controllers/firestore.controller.js
+++ app/controllers/firestore.controller.js
@@ -8,26 +8,13 @@ const db = new Firestore({
     keyFilename: servicePath
   });
 
-const add = async (req, res) => {
-    // const  { name, email, phone } = req.body;
-    const data = {
-        name: "Mykhailo",
-        email: "mykhailo@deargod.com",
-        phone: 666
-    }
-    console.log(data);
-
-    if (data) {
-        const usersCollection = db.collection('Users');
-        console.log("Collections users berhasil dibuat");
-    
-        const pengunjung = await usersCollection.doc("Pengunjung");
-        console.log("Dokumen Pengunjung berhasil ditambahkan");
-    
-        await pengunjung.set(data);
-        console.log("Data berhasil ditambahkan");
-    
-        res.json({message: "Data berhasil ditambahkan"});
+const add = async (data) => {
+    try {
+        const docRef = db.collection('Users').doc(data.id);
+        await docRef.set(data);
+        console.log('User added');
+    } catch (error) {
+        console.log('Error adding user', error);
     }
 }
 
